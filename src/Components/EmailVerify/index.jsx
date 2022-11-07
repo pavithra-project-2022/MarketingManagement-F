3fa35064fc9d404199724c97a3dd239f1da88a17
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-11-07 17:20:34 +0530

    commited

--- src/Components/EmailVerify/index.jsx
+++ src/Components/EmailVerify/index.jsx
@@ -1,42 +1,44 @@
 import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./styles.module.css";
 import { Fragment } from "../../../node_modules/react/cjs/react.production.min";
 import success from "../../image/success.png";
 
 const EmailVerify = () => {
-	const [validUrl, setValidUrl] = useState(true);
-	const param = useParams();
+  const param = useParams();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const verifyEmailUrl = async () => {
-			try {
-				const url = `https://mms-server.herokuapp.com/api/auth/${param.id}/verify/${param.token}`;
-				const { data } = await axios.get(url);
-				console.log(data);
-				setValidUrl(true);
-			} catch (error) {
-				console.log(error);
-				setValidUrl(false);
-			}
-		};
-		verifyEmailUrl();
-	}, [param]);
+  useEffect(() => {
+    const verifyEmailUrl = async () => {
+      try {
+        const url = `https://mms-server.herokuapp.com/api/auth/${param.id}/verify/${param.token}`;
+        const { data } = await axios.get(url);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    verifyEmailUrl();
+  }, [param]);
 
-	return (
-		<Fragment>
-			
-				<div className={styles.container}>
-					<img src={success} alt="success_img" className={styles.success_img} />
-					<h1>Email verified successfully</h1>
-					<Link to="/login">
-						<button className={styles.green_btn}>Login</button>
-					</Link>
-				</div>
-		
-		</Fragment>
-	);
+  const load = () => {
+    navigate("/login");
+    window.location.reload();
+  };
+
+  return (
+    <Fragment>
+      <div className={styles.container}>
+        <img src={success} alt="success_img" className={styles.success_img} />
+        <h1>Email verified successfully</h1>
+
+        <button className={styles.green_btn} onClick={load}>
+          Login
+        </button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default EmailVerify;
